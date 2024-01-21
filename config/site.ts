a2849f158b92ad8d693ec1921e770f6bcbec9f27
karimscoding <rejaulkarim66666@gmail.com>
2024-01-22 07:23:04 +0800

    ci: implement site comfig

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from '@/types';
+
+export const siteConfig: SiteConfig = {
+  name: 'dev-unity',
+  description: 'An open source application where developer helps developer',
+  url: 'http://localhost:3000',
+  ogImage: '',
+  links: {
+    twitter: '',
+    github: '',
+  },
+};
